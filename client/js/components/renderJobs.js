b908e2d903c3d74bd5cfb18beb2707c16a64bb6b
JeremyM <jeremymorkos@gmail.com>
2023-04-23 10:47:52 +1000

    front end display for add job complete

--- client/js/components/renderJobs.js
+++ client/js/components/renderJobs.js
@@ -1,4 +1,5 @@
 
+
 const jobContainer = document.createElement('div')
 jobContainer.id = 'jobListContainer'
 let isDisplayed = false
@@ -22,8 +23,8 @@ const displayJobList = () =>{
                 const location = job.location
                 const description = job.description
                 const jobUrl = job.job_url
-                const dueDate = job.due_date
-                const stages = job.stages
+                const dueDate = new Date(job.due_date)
+                const stage = job.stages
 
                 const container = document.createElement('div')
                 const ul = document.createElement('ul')
@@ -34,17 +35,43 @@ const displayJobList = () =>{
                 <li>Location: ${location}</li>
                 <li>Description: ${description}</li>
                 <li>Job URL: ${jobUrl}</li>
-                <li>Due date: ${dueDate}</li>
-                <li>Stages: ${stages}</li>
+                <li>Due date: ${dueDate.toLocaleDateString()}</li>
+                <li>Stage: ${stage}</li>
                 `
+
+                // const editButton = document.createElement("button");
+                // editButton.textContent = "Edit";
+                // editButton.addEventListener("click", () => {
+                //   return axios.get(`/jobs/${id}`)
+                //     .then((res) => {
+                //      (info)
+                //     })
+                // })
+                 
+                    
+                // const deleteButton = document.createElement("button");
+                // deleteButton.textContent = "Delete";
+                // deleteButton.addEventListener("click", () => {
+                //     return axios.delete(`/jobs/${id}`)
+                //     .then((res) => {
+                //     container.remove();
+                //     })
+
+                // })
+
                 container.appendChild(ul)
+                // container.appendChild(editButton)
+                // container.appendChild(deleteButton)
                 jobContainer.appendChild(container)
             })
             document.body.appendChild(jobContainer)
             isDisplayed = true
-        })
+            })
+        }
     }
-}        
+
+
+
 
 
 const jobsBtn = document.getElementById('jobs')
