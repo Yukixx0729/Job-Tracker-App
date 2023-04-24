1cd9a84f329ed487239ba59afecd75b55db22fdb
曾玉彬 <yuki@hitamaakirasair.lan>
2023-04-24 21:07:35 +1000

    Homepage with Due data

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -21,7 +21,73 @@ const renderHeader = () => {
 
 const renderQuote = () => {
   page.innerHTML = "";
-  axios.get("https://api.goprogram.ai/inspiration").then((res) => {
-    page.innerHTML = `${res.data.quote}  ----  ${res.data.author}`;
+  const quote = document.createElement("div");
+  quote.id = "quoteBox";
+  return axios.get("https://api.goprogram.ai/inspiration").then((res) => {
+    quote.innerHTML = `${res.data.quote}  ----  ${res.data.author}`;
+    page.appendChild(quote);
+    renderDueJobAndTodo();
   });
 };
+
+async function renderDueJobAndTodo() {
+  const container = document.createElement("div");
+  container.id = "dueDataBox";
+  const quote = document.getElementById("quoteBox");
+  quote.appendChild(container);
+  let dueData = [];
+  const dueDataJob = await axios
+    .get(`/jobs/1`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  const dueDataTodo = await axios
+    .get(`/todos/1`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  if (!dueDataJob && !dueDataTodo) {
+    const reminder = document.createElement("div");
+    reminder.innerHTML = "Nothing to worry about yet.";
+    container.appendChild(reminder);
+  } else {
+    if (dueDataJob.length === 1 && dueDataTodo.length === 1) {
+      dueData.push(dueDataJob);
+      dueData.push(dueDataTodo);
+    } else if (dueDataJob.length === 1 && dueDataTodo.length > 1) {
+      dueData.push(dueDataJob);
+      dueData = dueData.concat(dueDataTodo);
+    } else if (dueDataJob.length > 1 && dueDataTodo.length === 1) {
+      dueData.push(dueDataTodo);
+      dueData = dueData.concat(dueDataJob);
+    } else {
+      dueData = dueDataJob.concat(dueDataTodo);
+    }
+    console.log(dueData);
+    const sortedData = dueData.sort(
+      (a, b) => new Date(a["due_date"]) - new Date(b["due_date"])
+    );
+    // console.log(sortedData);
+    for (let due of sortedData) {
+      const dueItem = document.createElement("div");
+      dueItem.className = "dueItem";
+      if (Object.keys(due)[2] === "description") {
+        dueItem.innerHTML = `<p>${due.title}</p> <p> Priority : ${
+          due.priority
+        }</p><p>${due.due_date.substring(0, 10)}</p> `;
+      }
+      if (Object.keys(due)[2] === "company") {
+        dueItem.innerHTML = `<p>${due.company}</p> <p> ${
+          due.title
+        }</p> <p>${due.due_date.substring(0, 10)}</p>`;
+      }
+      container.appendChild(dueItem);
+    }
+  }
+}
